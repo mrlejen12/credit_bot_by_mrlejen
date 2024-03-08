bd35de00f33e2a7e08443bed316d920ca119267c
mrlejen12 <162735748+mrlejen12@users.noreply.github.com>
2024-03-08 21:30:05 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const info = {
     token: '' , //bot token
     private : true, //if you want bot just in your server edit private to true if you dont need this edit to true
-    servers : ["1203360440403361842","2","3"], // id your servers 
+    servers : ["1","2","3"], // id your servers 
     name:"test",//bot name
     state:"test"//bot state
 }
