04465941c3e42f6d632f17d4d682974ab6d58c64
mrlejen12 <162735748+mrlejen12@users.noreply.github.com>
2024-03-08 21:20:00 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const info = {
+    token: '' , //bot token
+    private : true, //if you want bot just in your server edit private to true if you dont need this edit to true
+    servers : ["1203360440403361842","2","3"], // id your servers 
+    name:"test",//bot name
+    state:"test"//bot state
+}
+
+
+module.exports = info;
+
